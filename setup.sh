cffb6c42c805763a82d740674b5fc330c9d8032c
MiTribunskiy <46776781+MiTribunskiy@users.noreply.github.com>
2020-06-06 07:09:29 +0300

    Fixed python version

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@ VIRTUAL_ENV=".env_fuzzy"
 if [ -d "$VIRTUAL_ENV" ]; then
   source "$VIRTUAL_ENV/bin/activate"
 else
-  python3.6 -m venv $VIRTUAL_ENV
+  python3 -m venv $VIRTUAL_ENV
   source "$VIRTUAL_ENV/bin/activate"
   pip install -r requirements/requirements.txt
 fi
